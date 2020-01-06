69ea38f903eb202b3255e29ed7d6e14fcfb080e4
wschwab <x@x.x>
2020-01-06 18:39:35 +0200

    checkbox, tasks, more

--- src/firebase.js
+++ src/firebase.js
@@ -2,13 +2,13 @@ import firebase from 'firebase/app'
 import 'firebase/firestore'
 
 const firebaseConfig = firebase.initializeApp({
-    apiKey: '',
-    authDomain: '',
-    databaseURL: '',
-    projectId: '',
-    storageBucket: '',
-    messageSenderId: '',
-    apiId: '',
+    apiKey: "AIzaSyCqxI57-ZDqiAfYWMrpqDCSgOHrEg47omo",
+    authDomain: "reacttodo-c6500.firebaseapp.com",
+    databaseURL: "https://reacttodo-c6500.firebaseio.com",
+    projectId: "reacttodo-c6500",
+    storageBucket: "reacttodo-c6500.appspot.com",
+    messagingSenderId: "789267445293",
+    appId: "1:789267445293:web:923ad11c225aed0f0e2aa4"
 })
 
 export {firebaseConfig as firebase }
---
+++ src/helpers/index.js
@@ -0,0 +1,5 @@
+import { collatedTasks } from '../constants'
+
+export const collatedTasksExists = selectedProject => {
+    collatedTasks.find(task => task.key === selectedProject)
+}
