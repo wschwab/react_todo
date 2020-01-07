5876387bac8847a80607236e63b53fb488477a15
wschwab <x@x.x>
2020-01-07 20:35:10 +0200

    started context, project component

---
+++ src/components/IndividualProject.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react'
+import { FaTrashAlt } from 'react-icons/fa'
+import { useProjectsValue, useSelectedProjectValue } from '../context'
+import { firebase }  from '../firebase'
+
+export const IndividualProject = ({ project }) => {
+    const [showConfirm, setShowConfirm] = useState(false)
+    const { projects, setProjects }= useProjectsValue()
+    const { setSelectedProject } = useSelectedProjectValue()
+
+    const deleteProject = docId => {
+        firebase.firestore().collection('projects').doc(docId)
+            .delete()
+            .then(() => {
+                setProjects([...projects])
+                setSelectedProject('INBOX')
+            })
+    }
+}
