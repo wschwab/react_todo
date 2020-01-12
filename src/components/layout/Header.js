b350b6736f7552dac434d7713743868a1f429c36
wschwab <x@x.x>
2020-01-12 18:44:27 +0200

    small changes, still broken

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,7 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FaPizzaSlice } from 'react-icons/fa'
+import { AddTask } from '../AddTask'
 
-export const Header = () => {
+export const Header = ({ darkMode, setDarkMode }) => {
+    const [shouldShowMain, setShouldShowMain] = useState(false)
+    const [showQuickAddTask, setShowQuickAddTask] = useState(false)
+
     return (
         <header className="header" data-testid="header">
             <nav>
@@ -10,15 +14,37 @@ export const Header = () => {
                 </div>
                 <div className="settings">
                     <ul>
-                        <li className="settings__add" data-testid="quick-add-task-action">
-                        +
+                        <li className="settings__add">
+                            <button
+                                data-testid="quick-add-task-action"
+                                type="button"
+                                onClick={() => {
+                                    setShowQuickAddTask(true)
+                                    setShouldShowMain(true)
+                                }}
+                            >
+                            +
+                            </button>
                         </li>
-                        <li className="settings__darkmode" data-testid="darkmode-action">
-                            <FaPizzaSlice />
+                        <li className="settings__darkmode">
+                            <button
+                                data-testid="darkmode-action"
+                                type="button"
+                                onClick={() => setDarkMode(!darkMode)}
+                            >
+                                <FaPizzaSlice />
+                            </button>
                         </li>
                     </ul>
                 </div>
             </nav>
+
+            <AddTask
+                showAddTask={false}
+                shouldShowMain={shouldShowMain}
+                showQuickAddTask={false}
+                showAddTaskMain={setShowQuickAddTask}
+            />
         </header>
     )
 }
