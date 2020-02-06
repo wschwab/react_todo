1d62644e1d2be00ab3b524ae20e58336e837e72b
wschwab <x@x.x>
2020-02-06 22:43:57 +0200

    tests for ProjectOverlay and Projects

---
+++ src/__tests__/Projects.spec.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import { render, cleanup, fireEvent } from '@testing-library/react'
+import { Projects } from '../components/Projects'
+
+beforeEach(cleanup)
+
+jest.mock('../context', () => ({
+    useSelectedProjectValue: jest.fn(() => ({
+        setSelectedProject: jest.fn(() => 'INBOX')
+    })),
+    useProjectsValue: jest.fn(() => ({
+        projects: [
+            {
+                name: 'say AARGH',
+                projectId: '1',
+                userId: '1',
+                docId: 'z9uTRdFaRZka4E13cIA3'
+            }
+        ]
+    }))
+}))
+
+describe('<ProjectOverlay />', () => {
+    afterEach(() => {
+        jest.clearAllMocks()
+    })
+
+    describe('Success', () => {
+        it('renders the projects', () => {
+            const { queryByTestId } = render(<Projects />)
+            expect(queryByTestId('project-action')).toBeTruthy()
+        })
+
+        it('renders the projects and selects using onClick', () => {
+            const { queryByTestId } = render(<Projects activeValue="1" />)
+            expect(queryByTestId('project-action')).toBeTruthy()
+
+            fireEvent.click(queryByTestId('project-action'))
+            expect(queryByTestId('project-action-parent').classList.contains('active')).toBeTruthy()
+        })
+
+        it('renders the projects and selects using onKeyDown', () => {
+            const { queryByTestId } = render(<Projects activeValue="1" />)
+            expect(queryByTestId('project-action')).toBeTruthy()
+
+            fireEvent.keyDown(queryByTestId('project-action'))
+            expect(queryByTestId('project-action-parent').classList.contains('active')).toBeTruthy()
+        })
+
+        it('renders the projects with no active value', () => {
+            const { queryByTestId } = render(<Projects activeValue="1" />)
+            expect(queryByTestId('project-action')).toBeTruthy()
+
+            fireEvent.keyDown(queryByTestId('project-action'))
+            expect(queryByTestId('project-action-parent').classList.contains('sidebar__project')).toBeTruthy()
+        })
+    })
+})
